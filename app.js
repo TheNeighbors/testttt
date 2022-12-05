6d9c4beea9e9a89ace07d2825c40c806c34de3eb
TheNeighbors <107973175+TheNeighbors@users.noreply.github.com>
2022-12-05 19:42:59 +0700

    .

--- app.js
+++ app.js
@@ -1,22 +1,26 @@
 const express = require('express');
 const morgan = require('morgan')
 const path = require('path');
-// const debug = require('debug')('app');
+const debug = require('debug')('app');
 const app = express();
 const host = 'localhost';
 const port = 3000;
 
 
 app.use(morgan('combined'))
-app.use(express.static(path.join(__dirname,'/public/')))
+app.use(express.static(path.join(__dirname, '/public/')))
+
+app.set("views", "./src/views")
+app.set("view engine", "ejs")
 
 app.get("/", (req, res) => {
-    res.send("Devskill...")
+
+    res.render('index', { username: 'ooooooooo' })
 })
 
 
 app.listen(port, () => {
-   console.log('Server Ready on PORT : ' + port)
+    console.log('Server Ready on PORT : ' + port)
 })
 
 
